41481c0d1f94bb2e94cf39cdf528db0c737b51d2
Abhimanyu Gangwar <ag15081993@gmail.com>
2020-07-27 23:18:37 +0100

    Attached sample dataset

--- daft_builder.py
+++ daft_builder.py
@@ -1,9 +1,12 @@
-from daftlistings import Daft
+from daftlistings import Daft, RentType
 
 daft = Daft()
+daft.set_listing_type(RentType.APARTMENT_TO_SHARE)
 listings = daft.search(fetch_all=False)
 
+dataset = ''
 for listing in listings:
-    print(listing.formalised_address)
-    print(listing.daft_link)
-    print(listing.price)
+    dataset += f'{listing.formalised_address.replace(",", " ")}, {listing.price}, {listing.description.replace(",", " ")}\n'
+
+with open('dataset.csv', 'w+') as f:
+    f.write(dataset)
