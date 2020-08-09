9f17643923735f697682ccfd9887ea0e62eb0044
Abhimanyu Gangwar <ag15081993@gmail.com>
2020-08-09 21:46:03 +0100

    Updated dataset for any rent for all listings

--- daft_builder.py
+++ daft_builder.py
@@ -1,12 +1,16 @@
 from daftlistings import Daft, RentType
+import os
 
 daft = Daft()
-daft.set_listing_type(RentType.APARTMENT_TO_SHARE)
-listings = daft.search(fetch_all=False)
+daft.set_listing_type(RentType.ANY)
+listings = daft.search()
 
-dataset = ''
-for listing in listings:
-    dataset += f'{listing.formalised_address.replace(",", " ")}, {listing.price}, {listing.description.replace(",", " ")}\n'
-
-with open('dataset.csv', 'w+') as f:
-    f.write(dataset)
+os.remove('dataset.csv')
+with open('dataset.csv', 'a+', encoding='utf-8') as f:
+    for i, listing in enumerate(listings):
+        print(f'Listing {i + 1}/{len(listings)}')
+        if listing.price:
+            trimmed_description = listing.description.replace(
+                ",", " ").replace("\n", " ")if listing.description else ""
+            f.write(
+                f'{listing.formalised_address.replace(",", " ")}, {listing.price}, {trimmed_description}\n')
